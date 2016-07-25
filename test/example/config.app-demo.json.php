068fee46771f650bef017e8d476b6499dc399d27
Josef Kufner <josef@kufner.cz>
2016-07-25 17:14:25 +0200

    Add demo app config

---
+++ test/example/config.app-demo.json.php
@@ -0,0 +1,20 @@
+{
+    "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
+    "flupdo": {
+        "driver": "sqlite",
+        "host": null,
+        "port": null,
+        "database": "./data/database.sqlite",
+        "username": null,
+        "password": null,
+        "log_query": null,
+        "log_explain": null
+    },
+    "smalldb": {
+        "base_dir": "./statemachine",
+        "cache_disabled": true
+    },
+    "auth": {
+        "class": "Smalldb\\StateMachine\\Auth\\CookieAuth"
+    }
+}
